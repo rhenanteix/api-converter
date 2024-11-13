36e7877bc4b7a48b028481e5157e786121b14bd8
Rhenan Silverio Teixeira <rhenancontato@gmail.com>
2024-11-12 23:33:39 -0300

    change yaml and outhers converts

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,29 @@
-use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use actix_cors::Cors;
 use csv::ReaderBuilder;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
+use serde_json::Value;
 use std::collections::HashMap;
 use std::io::Cursor;
 
-
-
+#[derive(Deserialize)]
+struct CsvData {
+    csvData: String,
+}
 
 #[derive(Serialize)]
 struct CSVRow(HashMap<String, String>);
 
-#[post("/csv_to_sql")]
-async fn csv_to_sql(csv_data: String) -> impl Responder {
-    let mut reader = ReaderBuilder::new()
-        .has_headers(true)
-        .from_reader(Cursor::new(csv_data));
-
-    // Lê os headers do CSV
-    let headers = match reader.headers() {
-        Ok(headers) => headers.clone(),
-        Err(_) => return HttpResponse::BadRequest().body("Erro ao processar CSV."),
-    };
-
-    let mut sql_statements = Vec::new();
-    
-    // Vamos gerar a instrução SQL de inserção baseada nas colunas do CSV
-    for result in reader.records() {
-        match result {
-            Ok(record) => {
-                // Convertendo StringRecord em um Vec<String>
-                let values: Vec<String> = record.iter()
-                    .map(|value| format!("'{}'", value)) // Adiciona aspas para os valores
-                    .collect();
-
-                // Gera a instrução SQL
-                let sql = format!(
-                    "INSERT INTO tabela ({}) VALUES ({});",
-                    headers.iter().collect::<Vec<&str>>().join(", "), // Usando join no vetor de headers
-                    values.join(", ")
-                );
-                sql_statements.push(sql);
-            }
-            Err(_) => return HttpResponse::BadRequest().body("Erro ao processar CSV."),
-        }
-    }
-
-    let sql_data = sql_statements.join("\n");
-
-    // Retornar o arquivo SQL como resposta
-    HttpResponse::Ok()
-        .insert_header(("Content-Disposition", "attachment; filename=output.sql"))
-        .content_type("application/sql")
-        .body(sql_data)
+#[get("/")]
+async fn hello_world() -> &'static str {
+    "Api em Rust rodando"
 }
 
+#[post("/convert/csv-to-json")]
+async fn csv_to_json(body: web::Json<CsvData>) -> impl Responder {
+    let csv_data = &body.csvData;
 
-#[post("/csv_to_json")]
-async fn csv_to_json(csv_data: String) -> impl Responder {
     let mut reader = ReaderBuilder::new()
         .has_headers(true)
         .from_reader(Cursor::new(csv_data));
@@ -83,29 +48,69 @@ async fn csv_to_json(csv_data: String) -> impl Responder {
         }
     }
 
-    // Serializar os dados JSON em uma string
     let json_data = json!(rows).to_string();
 
-    // Retornar o JSON como um arquivo para download
     HttpResponse::Ok()
         .insert_header(("Content-Disposition", "attachment; filename=output.json"))
         .content_type("application/json")
         .body(json_data)
 }
 
-#[post("/json_to_yaml")]
-async fn json_to_yaml(json_data: String) -> impl Responder {
-    match serde_json::from_str::<serde_json::Value>(&json_data) {
-        Ok(parsed_json) => {
-            // Converter JSON para YAML
-            match serde_yaml::to_string(&parsed_json) {
-                Ok(yaml) => HttpResponse::Ok().body(yaml),
-                Err(_) => HttpResponse::BadRequest().body("Erro ao converter JSON para YAML."),
+#[post("/convert/csv_to_sql")]
+async fn csv_to_sql(body: web::Json<CsvData>) -> impl Responder {
+    let csv_data = &body.csvData;
+
+    let mut reader = ReaderBuilder::new()
+        .has_headers(true)
+        .from_reader(Cursor::new(csv_data));
+
+    let headers = match reader.headers() {
+        Ok(headers) => headers.clone(),
+        Err(_) => return HttpResponse::BadRequest().body("Erro ao processar CSV."),
+    };
+
+    let mut sql_statements = Vec::new();
+    
+    for result in reader.records() {
+        match result {
+            Ok(record) => {
+                let values: Vec<String> = record.iter()
+                    .map(|value| format!("'{}'", value))
+                    .collect();
+
+                let sql = format!(
+                    "INSERT INTO tabela ({}) VALUES ({});",
+                    headers.iter().collect::<Vec<&str>>().join(", "),
+                    values.join(", ")
+                );
+                sql_statements.push(sql);
             }
+            Err(_) => return HttpResponse::BadRequest().body("Erro ao processar CSV."),
         }
-        Err(_) => HttpResponse::BadRequest().body("Erro ao processar JSON."),
+    }
+
+    let sql_data = sql_statements.join("\n");
+
+    HttpResponse::Ok()
+        .insert_header(("Content-Disposition", "attachment; filename=output.sql"))
+        .content_type("application/sql")
+        .body(sql_data)
+}
+
+#[post("/convert/json-to-yaml")]
+async fn json_to_yaml(body: web::Json<Value>) -> impl Responder {
+    match serde_yaml::to_string(&body) {
+        Ok(yaml_data) => {
+            HttpResponse::Ok()
+                .insert_header(("Content-Disposition", "attachment; filename=output.yaml"))
+                .content_type("application/x-yaml")
+                .body(yaml_data)
+        }
+        Err(_) => HttpResponse::BadRequest().body("Erro ao processar JSON para YAML."),
     }
 }
+
+
 
 
 #[actix_web::main]
@@ -118,8 +123,10 @@ async fn main() -> std::io::Result<()> {
 
         App::new()
             .wrap(cors)
+            .service(hello_world)
             .service(csv_to_json)
             .service(csv_to_sql)
+            .service(json_to_yaml)
     })
     .bind("127.0.0.1:8080")?
     .run()
